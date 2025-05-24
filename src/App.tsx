1d5f68a24431feb6e7ae565164f0b28a4fe86e58
Dolbezhnik <krendelalenkin@gmail.com>
2025-05-24 19:46:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    interface Photo {
+        id: number;
+        width: number;
+        height: number;
+        url: string;
+        alt: string | null;
+        avg_color: string | null;
+        photographer: string;
+        photographer_url: string;
+        photographer_id: number;
+        liked: boolean;
+        src: {
+            original: string;
+            large2x: string;
+            large: string;
+            medium: string;
+            small: string;
+            portrait: string;
+            landscape: string;
+            tiny: string;
+        };
+    }
+    const [bg, setBg] = useState('');
+    const [dataP, setDataP] = useState<Photo>();
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        async function getBg() {
+            const url =
+                'https://api.pexels.com/v1/search?query=natural%20mountains%20forest%20sea%20ocean%20sunset/?size=large&orientation=landscape&per_page=100';
+            const apiKey = import.meta.env.VITE_API_KEY;
+
+            return fetch(url, {
+                method: 'GET',
+                headers: {
+                    Authorization: apiKey, // Use "Bearer" or another scheme if required
+                },
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(
+                            `HTTP error! status: ${response.status}`
+                        );
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    const arrayData = Array.isArray(data.photos)
+                        ? data.photos
+                        : Object.values(data.photos);
+                    let photo;
+                    if (arrayData.length > 0) {
+                        const randomIndex = Math.floor(
+                            Math.random() * arrayData.length
+                        ); // Генерация случайного индекса
+                        photo = arrayData[randomIndex]; // Возвращаем случайный элемент
+                    } else {
+                        photo = arrayData[0]; // Или какое-то другое значение для пустого массива
+                    }
+                    setBg(photo?.src.original);
+                    setDataP(photo);
+                    console.log('Data received:', data);
+                    // Process your data here
+                })
+                .catch((error) => {
+                    console.error('Fetch error:', error);
+                    // Handle errors appropriately
+                });
+        }
+        getBg();
+    }, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []);
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+
+    const timeString = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }`;
+    return (
+        <>
+            <div className="background">
+                <img className="background-img" src={bg} alt="bg" />
+                <p>smth</p>
+                <div className="time">
+                    <h1 className="time__title">{timeString}</h1>
+                </div>
+                <div className="about-photo">
+                    <p className="author">Photographer: </p>
+                    <a href={dataP?.photographer_url} className="author-link">
+                        {dataP?.photographer}
+                    </a>
+                    <p className="original-img">Original: </p>
+                    <a href={dataP?.url} className="img-link">
+                        {dataP?.alt && dataP?.alt?.length > 20
+                            ? `${dataP?.alt?.slice(0, 20)}...`
+                            : dataP?.alt}
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
